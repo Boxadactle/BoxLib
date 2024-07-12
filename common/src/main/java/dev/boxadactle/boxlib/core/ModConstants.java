e56fb323c8df7afa74d523896bba568ca5792aae
Boxadactle <boxadactle@boxadactle.dev>
2024-07-11 20:49:44 -0700

    fix logger

--- common/src/main/java/dev/boxadactle/boxlib/core/ModConstants.java
+++ common/src/main/java/dev/boxadactle/boxlib/core/ModConstants.java
@@ -6,7 +6,7 @@ public class ModConstants {
 
     public static final String MOD_ID = "boxlib";
 
-    public static final String VERSION = "12.1.0";
+    public static final String VERSION = "11.3.1";
 
     public static final String WIKI = "https://github.com/Boxadactle/boxlib";
 }
