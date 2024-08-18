d903cddae6782678f7c8cc718d4a14fabcc5fb2a
Boxadactle <boxadactle@boxadactle.dev>
2024-08-17 22:01:21 -0700

    fix version

--- common/src/main/java/dev/boxadactle/boxlib/core/ModConstants.java
+++ common/src/main/java/dev/boxadactle/boxlib/core/ModConstants.java
@@ -6,7 +6,7 @@ public class ModConstants {
 
     public static final String MOD_ID = "boxlib";
 
-    public static final String VERSION = "12.1.2";
+    public static final String VERSION = "12.1.3";
 
     public static final String WIKI = "https://github.com/Boxadactle/boxlib";
 }
