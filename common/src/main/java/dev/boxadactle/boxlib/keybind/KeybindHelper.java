536c056c9131ed547107b52a9d1b6f5d03afafda
Boxadactle <boxadactle@boxadactle.dev>
2025-04-24 15:55:08 -0700

    fix forge bug

--- common/src/main/java/dev/boxadactle/boxlib/keybind/KeybindHelper.java
+++ common/src/main/java/dev/boxadactle/boxlib/keybind/KeybindHelper.java
@@ -2,6 +2,8 @@ package dev.boxadactle.boxlib.keybind;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import dev.boxadactle.boxlib.keybind.KeybindingImpl;
+import dev.boxadactle.boxlib.mixin.KeybindAccessor;
 import net.minecraft.client.KeyMapping;
 import org.apache.commons.lang3.NotImplementedException;
 
@@ -25,9 +27,8 @@ public class KeybindHelper {
      * @param key The keybinding to retrieve the bound key for.
      * @return The bound key.
      */
-    @ExpectPlatform
     public static InputConstants.Key getBoundKey(KeyMapping key) {
-        throw new NotImplementedException("KeybindHelper#getBoundKey");
+        return ((KeybindAccessor) key).getKey();
     }
 
 }
